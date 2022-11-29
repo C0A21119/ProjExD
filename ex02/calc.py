7fcaf716d3cc6ee0dbcaf443560a58d5775be0f3
miyajima <c0a211191a@edu.teu.ac.jp>
2022-11-29 14:28:16 +0900

    2_2

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,23 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
+def button_click(event):
+    btn =event.widget
+    i =btn["text"]
+    tkm.showinfo("",f"{i}ボタンがクリックされました。")
 root = tk.Tk()
 root.geometry("300x500")
+
+r,c = 0,0
+
+for i in range(9,-1,-1):
+    button = tk.Button(root,text=f"{i}",width=4,height=2,font=("",30))
+    button.grid(row = r, column = c)
+    button.bind("<1>",button_click)
+    c += 1
+    if c%3 ==0:
+        r+=1
+        c =0
+
+
 root.mainloop()
