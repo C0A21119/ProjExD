24b107c78a48ba38d5b56d28d5d57976cfab1e26
miyajima <c0a211191a@edu.teu.ac.jp>
2023-01-17 17:48:55 +0900

    変更

--- test2.py
+++ test2.py
@@ -1,106 +1,270 @@
-### インポート
+import pygame as pg
+import math
 import sys
-import random
+import tkinter as tk
 
-### 行動パターン
-act_dic = {"a":"攻撃", "b":"防御", "c":"魔法"}
+from pygame.locals import *
+import tkinter.messagebox as tkm
 
-### ダメージマトリックス
-arr = ([[3,3],[3,1],[2,4]],     # プレイヤー攻撃
-       [[1,3],[0,0],[3,0]],     # プレイヤー防御
-       [[4,2],[0,3],[1,1]])     # プレイヤー魔法
 
-### HP初期値
-play_hp = 20
-mons_hp = 20
+class Screen(pg.sprite.Sprite):#スクリーンクラス
+    def __init__(self, title, wh_pos:tuple, file_path):
+        pg.sprite.Sprite.__init__(self)
+        pg.display.set_caption(title)
+        self.sfc = pg.display.set_mode(wh_pos)
+        self.rect = self.sfc.get_rect()
+        self.bgi_sfc = pg.image.load(file_path)
+        self.bgi_rect = self.bgi_sfc.get_rect()
 
-### 素早さ
-play_agi = 8
-mons_agi = 7
+    def bilt(self):#背景の描画
+        self.sfc.blit(self.bgi_sfc, self.bgi_rect)
 
-### 関数定義
-def battle(arg):
 
-    ### ダメージマトリックスの配列
-    if arg == "プレイヤー":
-        f_idx = 1
-    else:
-        f_idx = 0
+class Paddle(pg.sprite.Sprite):#パドルクラス
+    def __init__(self, scrn:Screen):
+        pg.sprite.Sprite.__init__(self)
+        #長方形の作成
+        self.image = pg.Surface((100, 10))
+        self.image.set_colorkey((0, 0, 0))
+        pg.draw.rect(self.image, "blue", (0,0,100,10))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = scrn.rect.centerx
+        self.rect.bottom = scrn.rect.bottom
+        self.scrn = scrn
+        self.scrn.sfc.blit(self.image, self.rect)#描写
 
-    if   play_act == "攻撃" and mons_act == "攻撃":
-        f_damage = arr[0][0][f_idx]
-    elif play_act == "攻撃" and mons_act == "防御":
-        f_damage = arr[0][1][f_idx]
-    elif play_act == "攻撃" and mons_act == "魔法":
-        f_damage = arr[0][2][f_idx]
-    elif play_act == "防御" and mons_act == "攻撃":
-        f_damage = arr[1][0][f_idx]
-    elif play_act == "防御" and mons_act == "防御":
-        f_damage = arr[1][1][f_idx]
-    elif play_act == "防御" and mons_act == "魔法":
-        f_damage = arr[1][2][f_idx]
-    elif play_act == "魔法" and mons_act == "攻撃":
-        f_damage = arr[2][0][f_idx]
-    elif play_act == "魔法" and mons_act == "防御":
-        f_damage = arr[2][1][f_idx]
-    elif play_act == "魔法" and mons_act == "魔法":
-        f_damage = arr[2][2][f_idx]
+    def update(self):#アップデート関数
+        self.rect.centerx = pg.mouse.get_pos()[0]#マウスのx座標と同期する
+        self.rect.clamp_ip(self.scrn)#画面外に出ていかないように
 
-    ### ダメージを返す
-    return f_damage
+class Ball(pg.sprite.Sprite):#ボールクラス
+    speed = 5#スピード
+    #反射角
+    angle_left = 135
+    angle_right = 45
+    count = 3# 残基
 
-### 戦闘が終了するまでループ
-while True:
+    def __init__(self, paddle, scrn:Screen):
+        pg.sprite.Sprite.__init__(self)
+        #長方形の描画
+        self.image = pg.Surface((20, 20))
+        self.image.set_colorkey((0, 0, 0))
+        pg.draw.circle(self.image, "red", (10, 10), 10)
+        self.rect = self.image.get_rect()
+        self.paddle = paddle
+        #アップデートの切り替え
+        self.update = self.start
+        self.scrn = scrn
+        # ボールの速度
+        self.dx = 0
+        self.dy = 0
 
-    ### 行動入力
-    print(">>> 行動を選択してください")
-    print(">>> a:攻撃 b:防御 c:魔法 q:終了")
-    player = input(">>> ")
+    def start(self):#初期座標
+        self.rect.centerx = pg.mouse.get_pos()[0]
+        self.rect.bottom = self.paddle.rect.top
+        if pg.mouse.get_pressed()[0] == 1:#左クリックで発射
+            self.dx = 0
+            self.dy = -self.speed
+            self.update = self.move
 
-    ### 終了
-    if player == "q":
-        break
+    def move(self):#移動関数
+        if self.rect.bottom == self.scrn.rect.bottom:#落ちた場合
+            self.update = self.start
+            Ball.count -= 1
+        yoko,tate = check_bound(self.rect, self.scrn.rect)#バウンドチェック
+        self.dx = self.dx * yoko
+        self.dy = self.dy * tate
+        self.rect.centerx += self.dx
+        self.rect.centery += self.dy
+        self.rect.clamp_ip(self.scrn)#画面外に出ないように
 
-    ### 入力エラー
-    elif player not in list(act_dic.keys()):
-        print("＋＋ a:攻撃 b:防御 c:魔法 q:終了 を入力してください")
-        print()
-        continue
 
-    ### 行動保存
-    play_act = act_dic[player]
-    mons_act = act_dic[random.choice(list(act_dic.keys()))]
+class Block(pg.sprite.Sprite):#ブロッククラス
+    def __init__(self, scrn:Screen, x, y):
+        pg.sprite.Sprite.__init__(self)
+        #長方形描画
+        self.image = pg.Surface((40, 20))
+        self.image.set_colorkey((0, 0, 0))
+        pg.draw.rect(self.image, "blue", (0,0,30,10))
+        self.rect = self.image.get_rect()
+        #描画位置設定
+        self.rect.left = 5 + scrn.rect.left + x * self.rect.width
+        self.rect.top = 5 + scrn.rect.top + y * self.rect.bottom
 
-    ### 戦闘順決定
-    play_agi2 = play_agi * (1 + random.randrange(100) / 100)
-    mons_agi2 = mons_agi * (1 + random.randrange(100) / 100)
-    char_dic = {"プレイヤー":play_agi2, "モンスター":mons_agi2}
 
-    ### 行動順設定
-    for character in sorted(char_dic.items(), key=lambda arg: arg[1], reverse=True):
+class Sub_screen(): #スタート画面
+    def start(self):
+        root = tk.Tk()
+        root.title("start")
+        root.geometry("600x250")
 
-        ### 行動判定
-        damage = battle(character[0])
+        label = tk.Label(root, 
+                         text="ブロック崩しEX!!", 
+                         font=("",50)
+                        )
+        label.pack()
 
-        ### 行動結果を表示
-        if character[0] == "プレイヤー":
-            mons_hp -= damage
-            mons_hp = max(0, mons_hp)
-            print("＋＋ プレイヤーは{}を実行！ モンスターは{}のダメージ".format(play_act, damage))
-        else:
-            play_hp -= damage
-            play_hp = max(0, play_hp)
-            print("＋＋ モンスターは{}を実行！ プレイヤーは{}のダメージ".format(mons_act, damage))
+        label = tk.Label(root, 
+                         text=" ~ 目指せハイスコア!!! ~ ", 
+                         font=("",30)
+                        )
+        label.pack()
 
-        ### HP判定
-        if   mons_hp == 0:
-            print()
-            print("＋＋ モンスターを倒しました！ ＋＋")
-            sys.exit()
-        elif play_hp == 0:
-            print()
-            print("＋＋ プレイヤーは倒されました... ＋＋")
-            sys.exit()
+        #ルールボタン
+        def button_click():
+            tkm.showwarning("ルール","マウスパッドをタップでスタート!ポインターを左右に動かしてボールをブロックにぶつけよう!!")
+                
+        button = tk.Button(root, text="ルール", command=button_click)
+        button.pack()
+        
+        label = tk.Label(root, 
+                         text=" 右上の×ボタンをクリックしてゲームスタート!! ", 
+                         font=("",20)
+                        )
+        label.pack()
+        
+        root.mainloop()
+        
+    def end(self, Score):
+        root = tk.Tk()
+        root.title("お疲れ様")
+        root.geometry("300x100")
+        score = Score.score
+        if score > Score.HISCORE:#ハイスコア判定と書き込み
+            with open('ex06/text.txt', mode="w", encoding="UTF-8") as file:
+                file.write(f"{score}")
+        label = tk.Label(root,
+                        text=f"お疲れ様{score}点だよ\n前回までのハイスコアは\n{Score.HISCORE}点だよ",
+                        font=("", 20)
+                        )
+        label.pack()
+        root.mainloop()
+        
+def check_collision(balls, paddle, paddles, blocks):
+    oldblocks = len(blocks)
+    for ball in balls:
+        blocks_collided = pg.sprite.spritecollide(ball, blocks, True)
+        paddle_collided = pg.sprite.spritecollide(ball, paddles, False)
+        if blocks_collided:
+            score.add(oldblocks - len(blocks))#スコア加算
+        oldrect = ball.rect
+        oldrect = ball.rect
+        for block in blocks_collided:
+                # ボールが左から衝突
+                if oldrect.left < block.rect.left < oldrect.right < block.rect.right:
+                    ball.rect.right = block.rect.left
+                    ball.dx = -ball.dx
+                # ボールが右から衝突
+                if block.rect.left < oldrect.left < block.rect.right < oldrect.right:
+                    ball.rect.left = block.rect.right
+                    ball.dx = -ball.dx
+                # ボールが上から衝突
+                if oldrect.top < block.rect.top < oldrect.bottom < block.rect.bottom:
+                    ball.rect.bottom = block.rect.top
+                    ball.dy = -ball.dy
+                # ボールが下から衝突
+                if block.rect.top < oldrect.top < block.rect.bottom < oldrect.bottom:
+                    ball.rect.top = block.rect.bottom
+                    ball.dy = -ball.dy
 
-    ### 空行
-    print()
+        if paddle_collided:
+            (x1, y1) = (paddle.rect.left - ball.rect.width, ball.angle_left)
+            (x2, y2) = (paddle.rect.right, ball.angle_right)
+            x = ball.rect.left                          # ボールが当たった位置
+            y = (float(y2-y1)/(x2-x1)) * (x - x1) + y1  # 線形補間
+            angle = math.radians(y)                     # 反射角度
+            ball.dx = ball.speed * math.cos(angle)
+            ball.dy = -ball.speed * math.sin(angle)
+
+class Score():#スコアクラス
+    def __init__(self):
+        self.font = pg.font.SysFont("arial", 30)#フォント設定
+        self.score = 0
+        self.x = 10#x座標
+        self.y = 250#y座標
+        nums = ""
+        with open('ex06/text.txt', mode="r", encoding="UTF-8") as file:# ハイスコア読み込み
+            for num in file.readline():
+                nums += num
+        self.HISCORE = int(nums)
+
+    def draw(self, Ball, scrn:Screen):#スコア描画
+        text = self.font.render(f"life{Ball.count} SCORE{self.score}", True, (0,0,0))
+        scrn.blit(text,(self.x, self.y))
+
+    def add(self, x):#スコア加算
+        self.score += x*10
+
+
+
+
+
+def check_bound(obj_rect, scr_rect): #反射チェック関数
+    yoko,tate = +1,+1
+    if obj_rect.left == scr_rect.left or obj_rect.right == scr_rect.right:
+        yoko = -1#反転
+    if obj_rect.top == scr_rect.top:
+        tate = -1#反転
+    return yoko, tate
+
+
+def main():
+    scrn = Screen("ブロック崩し", (600, 600), "fig/haikei.jpg")
+    group = pg.sprite.OrderedUpdates()  # 描画用のスプライトグループ
+    blocks = pg.sprite.Group()       # ブロック衝突判定用のスプライトグループ
+    paddles = pg.sprite.Group()      #パドル衝突判定用のスプライトグループ
+    paddle = Paddle(scrn)#パドル描画
+    group.add(paddle)
+    paddles.add(paddle)
+    for x in range(0, 15):#ブロック描画
+        for y in range(0, 11):
+            black = Block(scrn, x, y)
+            group.add(black)
+            blocks.add(black)
+
+    balls = []
+    for i in range(2):#ボール複数追加
+        ball = Ball(paddle, scrn)
+        balls.append(ball)
+        group.add(ball)
+    clock = pg.time.Clock()
+    subscreen = Sub_screen()
+    subscreen.start()
+    score = Score()#スコア
+    poseFlag = False#ポーズフラグ
+
+    while True:
+        scrn.bilt()
+        clock.tick(60)      # フレームレート(60fps)
+        group.update()        # 全てのスプライトグループを更新
+        group.draw(scrn.sfc)    # 全てのスプライトグループを描画
+        score.draw(Ball, scrn.sfc)    # スコアを描画
+        check_collision(balls, paddle, paddles, blocks, score)#衝突判定
+        pg.display.update()
+        #ポーズ判定
+        while poseFlag:
+            for event in pg.event.get():
+                if event.type == KEYDOWN and event.key == K_SPACE:
+                    poseFlag = not poseFlag
+        #ライフ判定orブロックが全て消えたら
+        if Ball.count == 0 or len(blocks) == 0:
+            subscreen.end(score)
+            return
+        #イベント判定
+
+        for event in pg.event.get():
+            if event.type == QUIT:
+                subscreen.end(score)
+                return
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                subscreen.end(score)
+                return
+            if event.type == KEYDOWN and event.key == K_SPACE:
+                poseFlag = not poseFlag
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
